2962961ae1f62490e0c32a48a1f075fc10ac4b9e
adityatri <adityaatri@urbancompany.com>
2024-10-20 17:29:23 +0530

    Added Check-Compliance api

--- source/compliance-checker/constants.ts
+++ source/compliance-checker/constants.ts
@@ -1,8 +1,8 @@
 const COMPLIANCE_POLICY_URL = 'https://docs.stripe.com/treasury/marketing-treasury/';
 
-const HUGGING_FACE_API_KEY = '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';
+const HUGGING_FACE_API_KEY = '';
 
-const CLAUDE_API_KEY = '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';
+const CLAUDE_API_KEY = '';
 
 export {
     COMPLIANCE_POLICY_URL,
